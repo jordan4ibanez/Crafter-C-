3f7acbb1adfc137f58bcb2433689eaa74a922527
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-17 08:54:18 -0500

    Create a debug info stream with randomly assigned array data

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -2,11 +2,36 @@
 #include <iostream>
 #include <array>
 #include "debug/log.h"
+#include <random>
 
 
 //create random assigned data structure for testing
 std::array<uint8_t, 128> aChunkOfData;
 
 void testMyChunk(){
-    log("Yeah that's pretty chunky");
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(0, 255); // define the range
+
+    //randomly disperse data values into the chunk array
+    for (int i = 0; i < aChunkOfData.size(); i++){
+        aChunkOfData[i] = distr(gen);
+    }
+
+
+    //create an info stream of dispersed data
+    logHeader("Begin Chunk");
+
+    std::string infoStream("");
+
+    for (int i = 0; i < aChunkOfData.size(); i++){
+        if (i != 0){
+            infoStream += ",";
+        }
+        infoStream += std::to_string(aChunkOfData[i]);
+    }
+
+    log(infoStream);
+
+    logFooter();
 }
