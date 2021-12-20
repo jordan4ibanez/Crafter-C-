765ff7ec8fd4f825afe7490ad5453bcea917ff83
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-19 20:22:09 -0500

    Import openGL headers into mesh.cpp

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -1,10 +1,13 @@
 #define GLFW_INCLUDE_NONE
 #include <iostream>
 #include <random>
+#include <glfw-3.3.6/include/GLFW/glfw3.h>
+#include <glad/gl.h>
 #include "debug/log.h"
 #include "mesh.h"
 #include <glm/vec3.hpp>
 #include <tuple>
+
 
 //a magical immortal variable
 _Float32 color(0.3);
