81e37345265b2f7d5d0349726957bd2e27bc4bb6
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-12-19 20:22:49 -0500

    Clean out mesh class

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -9,33 +9,4 @@
 #include <tuple>
 
 
-//a magical immortal variable
-_Float32 color(0.3);
 
-//this doesn't do anything important at all, it was just to learn C++ with header files and cmake BOI
-//do not enable this if you are epileptic also I don't know how to spell thank you
-glm::vec3 myDebugTest(){
-
-    glm::vec3 test(1,2,3);
-
-    test.x = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-    test.y = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-    test.z = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
-    //color = r;
-
-    log(std::to_string(test.x) + " " + std::to_string(test.y) + " " + std::to_string(test.z));
-
-    //apparently vec3 is automatically deleted?
-
-    return(test);
-}
-
-//a very small test
-void mutableTest(int &i){
-    i = 5;
-}
-
-_Float32 getColor(){
-    return(color);
-}
--- src/mesh.h
+++ src/mesh.h
@@ -3,8 +3,3 @@
 #include <tuple>
 #include <glm/vec3.hpp>
 
- glm::vec3 myDebugTest();
-
-_Float32 getColor();
-
-void mutableTest(int &i);
